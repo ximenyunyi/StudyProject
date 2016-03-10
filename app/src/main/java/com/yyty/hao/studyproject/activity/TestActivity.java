8b67622ad27d64f0f54d1113cfb8cd198b087a88
gaohua <100239811@qq.com>
2016-03-10 14:57:55 +0800

    TestActivity
    新加了一个测试的Activty

---
+++ app/src/main/java/com/yyty/hao/studyproject/activity/TestActivity.java
@@ -0,0 +1,17 @@
+package com.yyty.hao.studyproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    /**
+     * 新建一个类
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
